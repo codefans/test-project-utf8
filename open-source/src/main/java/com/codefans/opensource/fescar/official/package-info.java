45ac4b75cfff3daa5db131246d2f4c5ccd91751f
codefans <363874279@qq.com>
2019-03-04 16:40:36 +0800

    添加fescar-samples-dubbo官方fescar示例

---
+++ open-source/src/main/java/com/codefans/opensource/fescar/official/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * @author: codefans
+ * @date: 2019-03-03 17:44:15
+ *
+ * fescar官方例子
+ *
+ * 源自：
+ *  https://github.com/fescar-group/fescar-samples
+ * 项目的dubbo模块
+ *
+ * 如何使用Fescar保证Dubbo微服务间的一致性
+ * http://dubbo.apache.org/zh-cn/blog/dubbo-fescar.html
+ *
+ */
+package com.codefans.opensource.fescar.official;
