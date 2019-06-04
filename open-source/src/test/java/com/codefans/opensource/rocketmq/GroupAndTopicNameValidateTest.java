4b1e76169054261aaa2565a5e877122b3767d152
codefans <363874279@qq.com>
2019-06-04 16:35:19 +0800

    groupName和topicName只能包含'%'、'|'、'_'、'-'四种特殊字符，其他只能是字母和数字

---
+++ open-source/src/test/java/com/codefans/opensource/rocketmq/GroupAndTopicNameValidateTest.java
@@ -0,0 +1,50 @@
+package com.codefans.opensource.rocketmq;
+
+import com.codefans.opensource.rocketmq.client.Validators;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.junit.Test;
+
+/**
+ * @author: codefans
+ * @date: 2019-06-04 16:16:40
+ */
+public class GroupAndTopicNameValidateTest {
+
+
+    @Test
+    public void groupNameValidateTest() {
+
+//        String groupName = "(fdfdf)";//报错
+//        String groupName = "^[%|a-zA-Z0-9_-]+$";//报错
+//        String groupName = "%|a-zA-Z0-9_-";//正常
+//        String groupName = "^dfdfd";//报错
+//        String groupName = "[dfdfd";//报错
+//        String groupName = "dfdfd]";//报错
+//        String groupName = "+dfdfd";
+//        String groupName = "$dfdfd";
+        String groupName = "#dfdfd";
+        try {
+            Validators.checkGroup(groupName);
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * group名和topic名只能包含'%'、'|'、'_'、'-'四个特殊字符，其他只能是数字和字母
+     */
+    @Test
+    public void topicNameValidateTest() {
+
+        String topicName = "%|_-fdkfdkfdlfd";
+        try {
+            Validators.checkTopic(topicName);
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
