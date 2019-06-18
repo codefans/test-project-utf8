4e06b94921bb075832fa9f5c26b6f02db1194d0b
codefans <363874279@qq.com>
2019-06-18 15:12:10 +0800

    Lamda表达式：list过滤出其中一条记录，如果记录不存在就new一个

--- basic-java/src/main/java8/stream/ListStreamTest.java
+++ basic-java/src/main/java8/stream/ListStreamTest.java
@@ -1,11 +1,9 @@
 package stream;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.collections.CollectionUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -16,11 +14,15 @@ public class ListStreamTest {
 
     public static void main(String[] args) {
         ListStreamTest listStreamTest = new ListStreamTest();
-//        listStreamTest.test();
+//        listStreamTest.filterSubList();
         listStreamTest.findOne();
     }
 
-    public void test() {
+    public void filterSubList() {
+
+        Byte b = 1;
+        Byte bb = 1;
+        System.out.println(b.equals(bb));
 
         List<String> list = new ArrayList<String>();
         list.add("111");
@@ -42,6 +44,15 @@ public class ListStreamTest {
         this.print(list);
         this.print(subList);
 
+        /**
+         * list过滤出一个子列表
+         */
+        List<String> subClsList = list.stream().filter(
+                sub -> sub.equals("111") || sub.equals("333")
+        ).collect(Collectors.toList());
+
+        System.out.println(subClsList.size());
+
     }
 
     public void findOne() {
@@ -53,10 +64,36 @@ public class ListStreamTest {
         list.add("444");
         list.add("555");
 
-        String str3 = list.stream().filter(
-                string -> Integer.parseInt(string) >= 333
-        ).iterator().next();
-        System.out.println(str3);
+        List<String> subList = list.stream().filter(
+                string -> Integer.parseInt(string) >= 666
+        ).collect(Collectors.toList());
+        if(CollectionUtils.isNotEmpty(subList)) {
+            System.out.println(subList.get(0));
+        }
+
+        /**
+         * list中找出符合条件的一条记录
+         */
+        String str = list.stream().filter(
+                string -> Integer.parseInt(string) >= 666
+        ).collect(Collectors.joining());
+        System.out.println("str=" + str);
+
+        List<SubClass> subClassList = new ArrayList<SubClass>();
+        subClassList.add(new SubClass("zhangsan", "123"));
+        subClassList.add(new SubClass("lisi", "456"));
+        subClassList.add(new SubClass("wangwu", "789"));
+        subClassList.add(new SubClass("zhaoliu", "012"));
+        subClassList.add(new SubClass("qianqi", "345"));
+
+        /**
+         * list过滤出其中的一条,如果没找到就new一个
+         */
+        SubClass subClass = subClassList.stream().filter(
+                sub -> sub.getName().equals("dfdfdf")
+        ).findFirst().orElse(new SubClass("dfdfdf", "123"));
+        System.out.println(subClass);
+
 
 
     }
@@ -72,4 +109,30 @@ public class ListStreamTest {
         System.out.println();
     }
 
+}
+
+class SubClass {
+
+    public SubClass(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+    String name;
+    String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
