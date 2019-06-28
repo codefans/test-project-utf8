d28f0ec2d2488a6c7911be548b0c98c00f03d6f5
codefans <363874279@qq.com>
2019-06-28 13:56:38 +0800

    Gson单元测试修改

--- open-source/src/test/java/com/codefans/opensource/json/performance/GsonPerformanceTest.java
+++ open-source/src/test/java/com/codefans/opensource/json/performance/GsonPerformanceTest.java
@@ -3,7 +3,7 @@ package com.codefans.opensource.json.performance;
 import org.junit.Test;
 
 /**
- * @author: caishengzhi
+ * @author: codefans
  * @date: 2018-05-23 09:32
  */
 public class GsonPerformanceTest {
@@ -11,8 +11,10 @@ public class GsonPerformanceTest {
     @Test
     public void gsonTest() {
 
-        GsonPerformance gsonPerformance = new GsonPerformance();
-        gsonPerformance.runTime();
+        String optType = "toJson";
+        GsonPerformance gson = new GsonPerformance(optType);
+        gson.runTime();
+        gson.costTime();
 
     }
 
