dac4f41d3176f75bb99a8bed56e09e91d69afc7c
codefans <363874279@qq.com>
2019-10-09 10:37:21 +0800

    为park和unpark添加注释

--- interview-case/src/main/java/com/codefans/interview/concurrent/crossexec/LockSupportImpl.java
+++ interview-case/src/main/java/com/codefans/interview/concurrent/crossexec/LockSupportImpl.java
@@ -11,6 +11,17 @@ public class LockSupportImpl {
     static Thread t1;
     static Thread t2;
 
+    /**
+     * LockSupport.park():
+     *     禁用当前线程，出于线程调度的目的，除非有许可证
+     *
+     * LockSupport.unpark(t2):
+     *     如果给定线程的许可证尚不可用，则使它的许可证可用。
+     *     如果线程被park阻塞，它将解除阻塞;否则，下一个对park的调用保证不会阻塞。
+     *     此操作不能保证完全有效，如果给定的线程尚未启动。
+     *
+     * @param args
+     */
     public static void main(String[] args) {
 
         char[] aI = "1234567890".toCharArray();
