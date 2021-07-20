9c4d0a418bb3c29054923622a6f25c48f377b1c1
codefans <363874279@qq.com>
2021-07-20 18:21:37 +0800

    获取链表长度

---
+++ interview-case/src/main/java/com/codefans/interview/algorithm/practise/GetLinkedListLength.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: GetLinkedListLength
+ * Author:   caishengzhi
+ * Date:     2021/7/20 18:11
+ * Description: 获取链表长度
+ */
+package com.codefans.interview.algorithm.practise;
+
+
+import com.codefans.interview.algorithm.common.ListNode;
+
+/**
+ *
+ * 获取链表长度
+ *
+ * @author: codefans
+ * @Date: 2021/07/20 18:11
+ * @since: 1.0.0
+ */
+public class GetLinkedListLength {
+
+    /**
+     * 获取链表长度
+     * @param head
+     * @return
+     */
+    public int getLen(ListNode head) {
+        int len = 0;
+        while(head != null && head.next != null) {
+            len += 2;
+            head = head.next.next;
+        }
+        if(head != null) {
+            len += 1;
+        }
+        return len;
+    }
+
+}
---
+++ interview-case/src/test/java/com/codefans/interview/algorithm/practise/GetLinkedListLengthTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: GetLinkedListLengthTest
+ * Author:   caishengzhi
+ * Date:     2021/7/20 18:14
+ * Description: 获取链表长度测试
+ */
+package com.codefans.interview.algorithm.practise;
+
+
+import com.codefans.interview.algorithm.common.ListNode;
+import com.codefans.interview.algorithm.common.ListNodeFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * 获取链表长度测试
+ *
+ * @author: codefans
+ * @Date: 2021/07/20 18:14
+ * @since: 1.0.0
+ */
+public class GetLinkedListLengthTest {
+
+    private GetLinkedListLength getLinkedListLength;
+
+    @Before
+    public void before() {
+        getLinkedListLength = new GetLinkedListLength();
+    }
+
+    @Test
+    public void getLenTest() {
+
+        int unitTestCount = 10;
+        for(int i = 1; i <= unitTestCount; i ++) {
+            ListNode head = ListNodeFactory.createListNode(i);
+            int len = getLinkedListLength.getLen(head);
+            System.out.println("i=" + i + ", len=" + len + ", i==len:" + (i == len));
+        }
+    }
+
+}
