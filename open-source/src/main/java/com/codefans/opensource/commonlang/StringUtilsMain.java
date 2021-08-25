1677f3db06832862a151327c410bb5b748407b55
codefans <363874279@qq.com>
2021-08-25 18:13:43 +0800

    StringUtils.join方法, 可以把一个列表以指定分隔符连接成一个字符串

---
+++ open-source/src/main/java/com/codefans/opensource/commonlang/StringUtilsMain.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: StringUtilsMain
+ * Author:   caishengzhi
+ * Date:     2021/8/25 18:11
+ * Description: StringUtils测试
+ */
+package com.codefans.opensource.commonlang;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * StringUtils测试
+ *
+ * @author: codefans
+ * @Date: 2021/08/25 18:11
+ * @since: 1.0.0
+ */
+public class StringUtilsMain {
+
+    public static void main(String[] args) {
+        StringUtilsMain sum = new StringUtilsMain();
+        sum.joinTest();
+    }
+
+    public void joinTest() {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("world");
+        System.out.println(StringUtils.join(list, ","));
+    }
+
+
+}
