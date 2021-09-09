3c619e36f3b857cb87e6308db3e9b038a500a648
codefans <363874279@qq.com>
2021-09-09 10:45:27 +0800

    No110 balanced binary tree

---
+++ interview-case/src/main/java/com/codefans/interview/algorithm/leetcode/tree/No110BalancedBinaryTree.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: No110BalancedBinaryTree
+ * Author:   caishengzhi
+ * Date:     2021/9/8 10:26
+ * Description: 判断是否是平衡二叉树
+ */
+package com.codefans.interview.algorithm.leetcode.tree;
+
+
+import com.codefans.interview.algorithm.common.TreeNode;
+
+/**
+ *
+ * 判断是否是平衡二叉树
+ * 平衡二叉树：即任意节点的左右子树高度差不超过1
+ *
+ * @author: codefans
+ * @Date: 2021/09/08 10:26
+ * @since: 1.0.0
+ */
+public class No110BalancedBinaryTree {
+
+    /**
+     * 判断是否是平衡二叉树
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        int leftDeepLen = maxDepth(root.left);
+        int rightDeepLen = maxDepth(root.right);
+        if(Math.abs(leftDeepLen - rightDeepLen) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) & isBalanced(root.right);
+    }
+
+    /**
+     * 求最大深度
+     * @param root
+     * @return
+     */
+    private int maxDepth(TreeNode root) {
+        int maxDepth = 0;
+        if(root == null) {
+            return maxDepth;
+        }
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+        if(leftMaxDepth > rightMaxDepth) {
+            maxDepth = leftMaxDepth + 1;
+        } else {
+            maxDepth = rightMaxDepth + 1;
+        }
+        //上面这个if-else等价于下面这句话
+//        maxDepth = Math.max(leftMaxDepth, rightMaxDepth) + 1;
+        //也等价于：
+//        maxDepth = (leftMaxDepth > rightMaxDepth ? leftMaxDepth : rightMaxDepth) + 1;
+        return maxDepth;
+    }
+}
---
+++ interview-case/src/test/java/com/codefans/interview/algorithm/tree/No110BalancedBinaryTreeTest.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: No110BalancedBinaryTreeTest
+ * Author:   caishengzhi
+ * Date:     2021/9/8 10:30
+ * Description: 判断是否是平衡二叉树
+ */
+package com.codefans.interview.algorithm.tree;
+
+
+import com.codefans.interview.algorithm.common.TreeNode;
+import com.codefans.interview.algorithm.common.TreeNodeFactory;
+import com.codefans.interview.algorithm.leetcode.tree.No110BalancedBinaryTree;
+import org.junit.Test;
+
+/**
+ *
+ * 判断是否是平衡二叉树
+ * [3,9,20,null,null,15,7] 返回true
+ * [1,2,2,3,3,null,null,4,4] 返回false
+ * @author: codefans
+ * @Date: 2021/09/08 10:30
+ * @since: 1.0.0
+ */
+public class No110BalancedBinaryTreeTest {
+
+    @Test
+    public void isBalancedTest() {
+
+//        Integer[] arr = new Integer[]{3,9,20,null,null,15,7}; //true
+//        Integer[] arr = new Integer[]{1,2,2,3,3,null,null,4,4}; //false
+//        Integer[] arr = new Integer[]{}; //true
+//        Integer[] arr = new Integer[]{1,null,3,2}; //false
+        Integer[] arr = new Integer[]{1,2,2,3,null,null,3,4,null,null,4}; //false
+
+        Integer[][] testArr = new Integer[][]{
+            {3,9,20,null,null,15,7},
+            {1,2,2,3,null,null,3,4,null,null,4},
+            {},
+            {1,null,3,2},
+            {1,2,2,3,null,null,3,4,null,null,4}
+        };
+        for(int i = 0; i < testArr.length; i ++) {
+            TreeNode treeNode = TreeNodeFactory.createTreeNode(testArr[i]);
+            TreeNodeFactory.printByLevel(treeNode);
+
+            No110BalancedBinaryTree no110BalancedBinaryTree = new No110BalancedBinaryTree();
+            boolean isBalanced = no110BalancedBinaryTree.isBalanced(treeNode);
+            System.out.println("isBalanced=" + isBalanced);
+        }
+
+    }
+
+}
