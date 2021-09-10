c1ff9308e36245a2e45d062c85232208bdd67041
codefans <363874279@qq.com>
2021-09-10 20:06:00 +0800

    新增No236LowestCommonAncestorOfAbinaryTree.java

---
+++ interview-case/src/main/java/com/codefans/interview/algorithm/leetcode/tree/No236LowestCommonAncestorOfAbinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: No236LowestCommonAncestorOfAbinaryTree
+ * Author:   caishengzhi
+ * Date:     2021/9/10 18:12
+ * Description: 二叉树的最近公共祖先
+ */
+package com.codefans.interview.algorithm.leetcode.tree;
+
+
+import com.codefans.interview.algorithm.common.TreeNode;
+
+/**
+ *
+ * 二叉树的最近公共祖先
+ *
+ * @author: codefans
+ * @Date: 2021/09/10 18:12
+ * @since: 1.0.0
+ */
+public class No236LowestCommonAncestorOfAbinaryTree {
+
+    /**
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode target = null;
+        
+        return target;
+    }
+
+}
---
+++ interview-case/src/test/java/com/codefans/interview/algorithm/tree/No236LowestCommonAncestorOfAbinaryTreeTest.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: No236LowestCommonAncestorOfAbinaryTreeTest
+ * Author:   caishengzhi
+ * Date:     2021/9/10 18:13
+ * Description: 二叉树的最近公共祖先
+ */
+package com.codefans.interview.algorithm.tree;
+
+
+import org.junit.Test;
+
+/**
+ *
+ * 二叉树的最近公共祖先
+ *
+ * @author: codefans
+ * @Date: 2021/09/10 18:13
+ * @since: 1.0.0
+ */
+public class No236LowestCommonAncestorOfAbinaryTreeTest {
+
+    @Test
+    public void lowestCommonAncestorTest() {
+
+    }
+
+
+}
