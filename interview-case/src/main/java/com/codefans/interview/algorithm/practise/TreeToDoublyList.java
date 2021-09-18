d84d99d4f97b0ae60b2562772a9408e3cad7c307
codefans <363874279@qq.com>
2021-09-18 19:01:13 +0800

    二叉搜索树与双向链表间的转换

---
+++ interview-case/src/main/java/com/codefans/interview/algorithm/practise/TreeToDoublyList.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: TreeToDoublyList
+ * Author:   caishengzhi
+ * Date:     2021/9/18 18:50
+ * Description: 二叉搜索树转双向链表
+ */
+package com.codefans.interview.algorithm.practise;
+
+
+import com.codefans.interview.algorithm.common.TreeNode;
+
+/**
+ *
+ * 二叉搜索树转双向链表
+ *
+ * @author: codefans
+ * @Date: 2021/09/18 18:50
+ * @since: 1.0.0
+ */
+public class TreeToDoublyList {
+
+    public TreeNode treeToDoublyList(TreeNode root) {
+        TreeNode doublyList = null;
+
+        return doublyList;
+    }
+}
---
+++ interview-case/src/test/java/com/codefans/interview/algorithm/practise/TreeToDoublyListTest.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C), 2015-2021, 京东
+ * FileName: TreeToDoublyListTest
+ * Author:   caishengzhi
+ * Date:     2021/9/18 18:51
+ * Description:
+ */
+package com.codefans.interview.algorithm.practise;
+
+
+import org.junit.Test;
+
+/**
+ *
+ * 二叉搜索树转双向链表
+ *
+ * @author: codefans
+ * @Date: 2021/09/18 18:51
+ * @since: 1.0.0
+ */
+public class TreeToDoublyListTest {
+
+    @Test
+    public void treeToDoublyListTest() {
+
+
+
+    }
+
+}
