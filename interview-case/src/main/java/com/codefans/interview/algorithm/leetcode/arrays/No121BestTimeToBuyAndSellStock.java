2ea0e75ac99c676231f1349c52b47a1132826273
codefans <363874279@qq.com>
2021-10-12 10:19:46 +0800

    买卖股票添加注释

--- interview-case/src/main/java/com/codefans/interview/algorithm/leetcode/arrays/No121BestTimeToBuyAndSellStock.java
+++ interview-case/src/main/java/com/codefans/interview/algorithm/leetcode/arrays/No121BestTimeToBuyAndSellStock.java
@@ -16,6 +16,18 @@ package com.codefans.interview.algorithm.leetcode.arrays;
  */
 public class No121BestTimeToBuyAndSellStock {
 
+    /**
+     * 如果我是在历史最低点买的股票就好了！在题目中，我们只要用一个变量记录一个历史最低价格 minprice，
+     * 我们就可以假设自己的股票是在那天买的。那么我们在第 i 天卖出股票能得到的利润就是 prices[i] - minprice。
+     * 因此，我们只需要遍历价格数组一遍，记录历史最低点，然后在每一天考虑这么一个问题：
+     * 如果我是在历史最低点买进的，那么我今天卖出能赚多少钱？当考虑完所有天数之时，我们就得到了最好的答案。
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * 
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
         int minprice = Integer.MAX_VALUE;
         int maxprofit = 0;
