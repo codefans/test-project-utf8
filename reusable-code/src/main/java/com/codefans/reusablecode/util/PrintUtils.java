4a4797c8c16473f2a27d1b51c2b09450a74e790a
codefans <363874279@qq.com>
2021-10-20 09:55:41 +0800

    新增打印整型数组的方法

--- reusable-code/src/main/java/com/codefans/reusablecode/util/PrintUtils.java
+++ reusable-code/src/main/java/com/codefans/reusablecode/util/PrintUtils.java
@@ -8,6 +8,7 @@
 package com.codefans.reusablecode.util;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -34,6 +35,31 @@ public class PrintUtils {
             }
         } else {
             System.out.println("list为空！！！");
+        }
+    }
+
+    /**
+     * 打印arr
+     * @param arr
+     */
+    public static final void printStrArray(String[] arr) {
+        printStrList(Arrays.asList(arr));
+    }
+
+    /**
+     * 打印arr
+     * @param arr
+     */
+    public static final void printIntArray(int[] arr) {
+        if(arr != null && arr.length > 0) {
+            for(int i = 0; i < arr.length; i ++) {
+                if(i != 0) {
+                    System.out.print(", ");
+                }
+                System.out.print(arr[i]);
+            }
+        } else {
+            System.out.println("arr为空！！！");
         }
     }
 
