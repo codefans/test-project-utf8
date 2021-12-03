e40fa51eef33a29dc08b8a441972e89554c41458
codefans <363874279@qq.com>
2021-12-03 14:48:08 +0800

    PriorityQueue测试

---
+++ basic-java/src/test/java/com/codefans/basicjava/java/util/PriorityQueueTest.java
@@ -0,0 +1,96 @@
+package com.codefans.basicjava.java.util;
+
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * PriorityQueue优先队列测试类
+ *
+ * @author: codefans
+ * @Date: 2021/12/03 13:59
+ * @since: 1.0.0
+ */
+public class PriorityQueueTest {
+
+    @Test
+    public void offerTest() {
+
+        /**
+         * 默认是小顶堆, 最小值在第一个位置
+         */
+        PriorityQueue<Integer> queue = new PriorityQueue<>(2);
+        queue.add(12);
+        queue.add(6);
+        queue.add(24);
+        queue.add(8);
+        queue.add(7);
+        queue.add(10);
+        /**
+         * 实际在队列中的顺序是：6、7、10、12、8、24
+         */
+
+//        queue.add(1);
+//        queue.add(3);
+//        queue.add(2);
+
+        System.out.println("size=" + queue.size());
+
+        /**
+         * 如果连续调用6次poll()方法，输出的值依次为：
+         *     6、7、8、10、12、24
+         */
+        this.print(queue);
+
+    }
+
+    /**
+     * 大顶堆
+     */
+    @Test
+    public void bigHeapTest() {
+        IntegerComparetor comparetor = new IntegerComparetor();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(2, comparetor);
+        queue.add(12);
+        queue.add(6);
+        queue.add(24);
+        queue.add(8);
+        queue.add(7);
+        queue.add(10);
+        /**
+         * 实际在队列中的顺序是：6、7、10、12、8、24
+         */
+
+//        queue.add(1);
+//        queue.add(3);
+//        queue.add(2);
+
+        System.out.println("size=" + queue.size());
+        this.print(queue);
+
+    }
+
+    class IntegerComparetor implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            int result = -2;
+            if(o1 > o2) {
+                result = -1;
+            } else if(o1 == o2) {
+                result = 0;
+            } else {
+                result = 1;
+            }
+            return result;
+        }
+    }
+    private void print(PriorityQueue<Integer> queue) {
+        int size = queue.size();
+        for(int i = 0; i < size; i ++) {
+            System.out.println(queue.poll());
+        }
+    }
+
+}
