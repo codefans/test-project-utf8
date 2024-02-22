e8fe810d13d81ca2398c25dbbcd8122cabaa256e
codefans <363874279@qq.com>
2024-02-22 16:25:10 +0800

    common-lang3包中StringUtils.capitalize方法将字符串首字符转为大写

--- open-source/src/main/java/com/codefans/opensource/commonlang/StringUtilsMain.java
+++ open-source/src/main/java/com/codefans/opensource/commonlang/StringUtilsMain.java
@@ -25,7 +25,13 @@ public class StringUtilsMain {
 
     public static void main(String[] args) {
         StringUtilsMain sum = new StringUtilsMain();
-        sum.joinTest();
+        sum.allTest();
+    }
+
+    public void allTest() {
+        this.joinTest();
+        this.capitalizeTest();
+        this.uncapitalizeTest();
     }
 
     public void joinTest() {
@@ -35,5 +41,21 @@ public class StringUtilsMain {
         System.out.println(StringUtils.join(list, ","));
     }
 
+
+    public void capitalizeTest() {
+        System.out.println(StringUtils.capitalize(null)); //输出：null
+        System.out.println(StringUtils.capitalize("")); //输出：""
+        System.out.println(StringUtils.capitalize("cat")); //输出："Cat"
+        System.out.println(StringUtils.capitalize("cAt")); //输出："CAt"
+        System.out.println(StringUtils.capitalize("'cat'")); //输出："'cat'"
+    }
+
+    public void uncapitalizeTest() {
+        System.out.println(StringUtils.uncapitalize(null)); //输出：null
+        System.out.println(StringUtils.uncapitalize("")); //输出：""
+        System.out.println(StringUtils.uncapitalize("cat")); //输出："cat"
+        System.out.println(StringUtils.uncapitalize("Cat")); //输出："cat"
+        System.out.println(StringUtils.uncapitalize("CAT")); //输出："cAT"
+    }
 
 }
